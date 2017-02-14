44ee7dbc0121d434093a4d4f2b4562030ae5dd26
mindsear <xxusername12843@yahoo.ro>
2017-02-14 23:20:00 +0200

    Update unit_flags.Designer.cs
    Check items with a single click

--- SpawnCreator/unit_flags.Designer.cs
+++ SpawnCreator/unit_flags.Designer.cs
@@ -55,6 +55,7 @@
             // 
             // checkedListBox1
             // 
+            this.checkedListBox1.CheckOnClick = true;
             this.checkedListBox1.FormattingEnabled = true;
             this.checkedListBox1.Items.AddRange(new object[] {
             "UNIT_FLAG_SERVER_CONTROLLED",
